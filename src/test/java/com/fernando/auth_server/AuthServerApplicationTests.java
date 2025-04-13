e37c867cebd79b2c399a2da71d8a5cef45c6729a
Fernando <asialer05@hotmail.com>
2025-04-13 16:03:06 -0500

    proyecto base

---
+++ src/test/java/com/fernando/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fernando.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO document why this method is empty
+	}
+
+}
