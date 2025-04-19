2b3437a6fbbc921980457ef09692b9dbfd38ed97
Fernando <asialer05@hotmail.com>
2025-04-18 22:46:58 -0500

    drop code comment and classes don't used

--- src/main/java/com/fernando/auth_server/config/CorsConfig.java
+++ src/main/java/com/fernando/auth_server/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
@@ -16,8 +18,7 @@ public class CorsConfig {
         cors.addAllowedHeader("*");
         cors.addAllowedMethod("*");
         cors.setAllowCredentials(true);
-        //cors.addAllowedOrigin("*");
-        cors.addAllowedOrigin("http://localhost:4200/");
+        cors.setAllowedOrigins(List.of("http://127.0.0.1:4200","http://localhost:4200"));
         source.registerCorsConfiguration("/**",cors);
         return source;
     }
